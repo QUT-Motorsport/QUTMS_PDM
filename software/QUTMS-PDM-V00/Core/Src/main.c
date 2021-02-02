fb7fad7089d580a121814e3949ca46ece5db5103
Tristan <smithtristan@hotmail.com>
2021-02-02 14:45:31 +1000

    update to get data off bmi160 sensors
    Added and implement libraries for BMI160 and currently it reads data from accelorometer and gyroscope and outputs it to UART

--- software/QUTMS-PDM-V00/Core/Src/main.c
+++ software/QUTMS-PDM-V00/Core/Src/main.c
@@ -29,6 +29,7 @@
 #include "usart.h"
 #include "usb_device.h"
 #include "gpio.h"
+#include "bmi160_Bosch.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -47,6 +48,8 @@
 #define NUM_CARDS_USED 8
 #define NUM_CARDS 8
 #define NUM_CHANNELS 4
+
+#define TIMEOUT_MINUTE 60000
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -88,14 +91,22 @@ void CAN_set_pwm_duty_cycle(uint8_t channel, uint8_t duty_cycle);
 
 /* USER CODE END 0 */
 
+/* USER CODE BEGIN 1 */
+// Writing functions for function pointers
+
+int8_t bmi_read_i2c(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data,
+		uint16_t len);
+int8_t bmi_write_i2c(uint8_t dev_addr, uint8_t reg_addr, uint8_t *buffer,
+		uint16_t len);
+int8_t bmi_delay_i2c(uint32_t period);
+
+/* USER CODE END 1 */
+
 /**
  * @brief  The application entry point.
  * @retval int
  */
 int main(void) {
-	/* USER CODE BEGIN 1 */
-
-	/* USER CODE END 1 */
 
 	/* MCU Configuration--------------------------------------------------------*/
 
@@ -144,12 +155,14 @@ int main(void) {
 	uint16_t RxSPIBuffer[1] = { 0 };
 	Tx16SPIBuffer[0] = 0xFFFF;
 	RxSPIBuffer[0] = 0xFFFF;
-	HAL_SPI_TransmitReceive(&hspi2, (uint8_t*) Tx16SPIBuffer, (uint8_t*) RxSPIBuffer, 1, 1000);
+	HAL_SPI_TransmitReceive(&hspi2, (uint8_t*) Tx16SPIBuffer,
+			(uint8_t*) RxSPIBuffer, 1, 1000);
 
 	//  Set DCR register, with SWR set to 0 - bit needs to be set for writing a register
 	uint8_t DCR = 0x00;
 	for (int i = 0; i < NUM_CARDS_USED; i++) {
-		receiveBuff = BTS7XX_WriteRegister(&hspi2, BTS7XX_WRITE_DCR_COMMAND, &DCR, card_ports[i], card_pins[i]);
+		receiveBuff = BTS7XX_WriteRegister(&hspi2, BTS7XX_WRITE_DCR_COMMAND,
+				&DCR, card_ports[i], card_pins[i]);
 	}
 
 	//  BTS7XX_OCR_OBJ ocrObj;
@@ -171,10 +184,14 @@ int main(void) {
 	//Test by setting all states high
 //	set_channel_states(UINT32_MAX);
 
-	uint32_t init_startup_id = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0, CAN_TYPE_RECEIVE, 0x00, 0x0);
-	uint32_t setchannelID = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0, CAN_TYPE_RECEIVE, 0x02, 0x00);
-	uint32_t requestDutyCycleID = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0, CAN_TYPE_RECEIVE, 0x3, 0x0);
-	uint32_t setDutyCycleID = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0, CAN_TYPE_RECEIVE, 0x4, 0x0);
+	uint32_t init_startup_id = Compose_CANId(CAN_PRIORITY_NORMAL,
+			CAN_SRC_ID_PDM, 0x0, CAN_TYPE_RECEIVE, 0x00, 0x0);
+	uint32_t setchannelID = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM,
+			0x0, CAN_TYPE_RECEIVE, 0x02, 0x00);
+	uint32_t requestDutyCycleID = Compose_CANId(CAN_PRIORITY_NORMAL,
+			CAN_SRC_ID_PDM, 0x0, CAN_TYPE_RECEIVE, 0x3, 0x0);
+	uint32_t setDutyCycleID = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM,
+			0x0, CAN_TYPE_RECEIVE, 0x4, 0x0);
 
 	// setup messages
 	PDM_Heartbeat_t heartbeat_msg;
@@ -187,32 +204,34 @@ int main(void) {
 	uint32_t txMailbox = 0;
 
 	// startup sequence
+	/*
+	 // wait for initiate startup
+	 uint8_t wait_flag = 0;
+	 while (!wait_flag) {
+	 while (HAL_CAN_GetRxFifoFillLevel(&hcan, CAN_RX_FIFO0) > 0) {
+	 // pull msg out
+	 CAN_MSG_Generic_t msg;
+	 HAL_CAN_GetRxMessage(&hcan, CAN_RX_FIFO0, &(msg.header), msg.data);
 
-	// wait for initiate startup
-	uint8_t wait_flag = 0;
-	while (!wait_flag) {
-		while (HAL_CAN_GetRxFifoFillLevel(&hcan, CAN_RX_FIFO0) > 0) {
-			// pull msg out
-			CAN_MSG_Generic_t msg;
-			HAL_CAN_GetRxMessage(&hcan, CAN_RX_FIFO0, &(msg.header), msg.data);
+	 // PDM_SetChannelStates
+	 if (msg.header.ExtId == init_startup_id) {
+	 wait_flag = 1;
+	 break;
+	 }
 
-			// PDM_SetChannelStates
-			if (msg.header.ExtId == init_startup_id) {
-				wait_flag = 1;
-				break;
-			}
+	 }
 
-		}
-
-		HAL_Delay(1);
-	}
+	 HAL_Delay(1);
+	 }
+	 */
 
 	// send startup ok
 	current_state = read_channel_states();
 	PDM_StartupOk_t startupOk_msg = Compose_PDM_StartupOk(current_state);
 	header.ExtId = startupOk_msg.id;
 	header.DLC = sizeof(startupOk_msg.data);
-	result = HAL_CAN_AddTxMessage(&hcan, &header, startupOk_msg.data, &txMailbox);
+	result = HAL_CAN_AddTxMessage(&hcan, &header, startupOk_msg.data,
+			&txMailbox);
 
 	// start heartbeat timer
 	if (HAL_TIM_Base_Start_IT(&htim2) != HAL_OK) {
@@ -231,9 +250,74 @@ int main(void) {
 
 	/* USER CODE END 2 */
 
+
+	/* BMI160 INITIALIZATION START */
+
+	struct bmi160_dev sensor;
+
+	sensor.id = BMI160_I2C_ADDR;
+	sensor.interface = BMI160_I2C_INTF;
+	sensor.read = bmi_read_i2c;
+	sensor.write = bmi_write_i2c;
+	sensor.delay_ms = bmi_delay_i2c;
+
+	int8_t rslt = BMI160_OK;
+	rslt = bmi160_init(&sensor);
+
+	rslt = BMI160_OK;
+
+	/* Select the Output data rate, range of accelerometer sensor */
+	sensor.accel_cfg.odr = BMI160_ACCEL_ODR_1600HZ;
+	sensor.accel_cfg.range = BMI160_ACCEL_RANGE_2G;
+	sensor.accel_cfg.bw = BMI160_ACCEL_BW_NORMAL_AVG4;
+
+	/* Select the power mode of accelerometer sensor */
+	sensor.accel_cfg.power = BMI160_ACCEL_NORMAL_MODE;
+
+	/* Select the Output data rate, range of Gyroscope sensor */
+	sensor.gyro_cfg.odr = BMI160_GYRO_ODR_3200HZ;
+	sensor.gyro_cfg.range = BMI160_GYRO_RANGE_2000_DPS;
+	sensor.gyro_cfg.bw = BMI160_GYRO_BW_NORMAL_MODE;
+
+	/* Select the power mode of Gyroscope sensor */
+	sensor.gyro_cfg.power = BMI160_GYRO_NORMAL_MODE;
+
+	/* Set the sensor configuration */
+	rslt = bmi160_set_sens_conf(&sensor);
+
+	/* BMI160 INITIALIZATION END */
+
+
+	/* After the above function call, accel and gyro parameters in the device structure
+	 are set with default values, found in the datasheet of the sensor */
+
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
+	int i = 0;
+
+	char msg[100];
+
 	while (1) {
+
+		int8_t rslt = BMI160_OK;
+		struct bmi160_sensor_data accel;
+		struct bmi160_sensor_data gyro;
+
+		/* To read only Accel data */
+		rslt = bmi160_get_sensor_data(BMI160_ACCEL_SEL, &accel, NULL, &sensor);
+
+		//sprintf(msg, "r: %d, a: (%d,\t%d,\t%d).\r\n", rslt, accel.x,accel.y,accel.z);
+		//HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen((char*) msg),HAL_MAX_DELAY);
+		i++;
+
+		HAL_Delay(100);
+
+		/* To read only Gyro data */
+		rslt = bmi160_get_sensor_data(BMI160_GYRO_SEL, NULL, &gyro, &sensor);
+		sprintf(msg, "r: %d, a: (%d,\t%d,\t%d).\r\n", rslt, gyro.x,gyro.y,gyro.z);
+				HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen((char*) msg),
+				HAL_MAX_DELAY);
+
 		// read current state
 		current_state = read_channel_states();
 
@@ -242,7 +326,8 @@ int main(void) {
 			heartbeat_msg = Compose_PDM_Heartbeat(current_state);
 			header.ExtId = heartbeat_msg.id;
 			header.DLC = sizeof(heartbeat_msg.data);
-			result = HAL_CAN_AddTxMessage(&hcan, &header, heartbeat_msg.data, &txMailbox);
+			result = HAL_CAN_AddTxMessage(&hcan, &header, heartbeat_msg.data,
+					&txMailbox);
 			send_heartbeat = 0;
 		}
 
@@ -263,10 +348,12 @@ int main(void) {
 				uint8_t channel = 0;
 				Parse_PDM_RequestDutyCycle(msg.data, &channel);
 
-				transmit_cycle_msg = Compose_PDM_TransmitDutyCycle(channel, user_dutycycles[channel]);
+				transmit_cycle_msg = Compose_PDM_TransmitDutyCycle(channel,
+						user_dutycycles[channel]);
 				header.ExtId = transmit_cycle_msg.id;
 				header.DLC = sizeof(transmit_cycle_msg.data);
-				result = HAL_CAN_AddTxMessage(&hcan, &header, transmit_cycle_msg.data, &txMailbox);
+				result = HAL_CAN_AddTxMessage(&hcan, &header,
+						transmit_cycle_msg.data, &txMailbox);
 
 			} else if (msg.header.ExtId == setDutyCycleID) {
 				uint8_t channel = 0;
@@ -297,7 +384,8 @@ void SystemClock_Config(void) {
 	/** Initializes the RCC Oscillators according to the specified parameters
 	 * in the RCC_OscInitTypeDef structure.
 	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI
+			| RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
 	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
 	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
 	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -311,7 +399,8 @@ void SystemClock_Config(void) {
 	}
 	/** Initializes the CPU, AHB and APB buses clocks
 	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -320,8 +409,9 @@ void SystemClock_Config(void) {
 	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
 		Error_Handler();
 	}
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_USART2 | RCC_PERIPHCLK_I2C1
-			| RCC_PERIPHCLK_RTC | RCC_PERIPHCLK_ADC12;
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB
+			| RCC_PERIPHCLK_USART2 | RCC_PERIPHCLK_I2C1 | RCC_PERIPHCLK_RTC
+			| RCC_PERIPHCLK_ADC12;
 	PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
 	PeriphClkInit.Adc12ClockSelection = RCC_ADC12PLLCLK_DIV1;
 	PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
@@ -333,6 +423,57 @@ void SystemClock_Config(void) {
 }
 
 /* USER CODE BEGIN 4 */
+
+int8_t bmi_read_i2c(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data,
+		uint16_t len) {
+	uint8_t address = dev_addr << 1 | 0;
+	HAL_StatusTypeDef result = HAL_I2C_Master_Transmit(&hi2c1, address,
+			&reg_addr, 1,
+			TIMEOUT_MINUTE);
+
+	if (result != HAL_OK) {
+		// unable to send read request correctly
+		return result;
+	} else {
+		return HAL_I2C_Master_Receive(&hi2c1, address, data, len,
+				TIMEOUT_MINUTE);
+	}
+}
+
+int8_t bmi_write_i2c(uint8_t dev_addr, uint8_t reg_addr, uint8_t *buffer,
+		uint16_t len) {
+
+	uint8_t address = dev_addr << 1 | 0;
+
+	// no crc as chip doesn't support it
+
+	// transmit register as first byte, then all of buffer according to length
+
+	uint8_t dataLength = 1 + len;
+	uint8_t *data = (uint8_t*) calloc(dataLength, sizeof(uint8_t));
+	data[0] = reg_addr;
+
+	memcpy(data + 1, buffer, len);
+
+	HAL_StatusTypeDef result = HAL_I2C_Master_Transmit(&hi2c1, address, data,
+			dataLength, TIMEOUT_MINUTE);
+
+	// clean up dynamic memory
+	free(data);
+
+	return result;
+}
+
+int8_t bmi_delay_i2c(uint32_t period) {
+	// delay ms function
+	HAL_Delay(period);
+}
+
+/* USER CODE END 4 */
+
+
+/* USER CODE BEGIN 5 */
+
 void CAN_set_channel_states(uint32_t new_state) {
 	uint32_t prim = __get_PRIMASK();
 
@@ -385,20 +526,22 @@ void set_channel_states(uint32_t powerChannels) {
 	// set states physically
 	for (int i = 0; i < NUM_CARDS_USED; i++) {
 		uint8_t value = (current_state >> (i * 4)) & 0xF;
-		BTS7XX_WriteRegister(&hspi2, BTS7XX_WRITE_OUT_COMMAND, &value, card_ports[i], card_pins[i]);
+		BTS7XX_WriteRegister(&hspi2, BTS7XX_WRITE_OUT_COMMAND, &value,
+				card_ports[i], card_pins[i]);
 	}
 }
 
 uint32_t read_channel_states() {
 	uint32_t channel_values = 0;
 	for (int i = 0; i < NUM_CARDS_USED; i++) {
-		uint8_t result = BTS7XX_ReadRegister(&hspi2, BTS7XX_READ_OUT_COMMAND, card_ports[i], card_pins[i]);
+		uint8_t result = BTS7XX_ReadRegister(&hspi2, BTS7XX_READ_OUT_COMMAND,
+				card_ports[i], card_pins[i]);
 		channel_values |= (result & 0xF) << (i * 4);
 	}
 	return channel_values;
 }
 
-/* USER CODE END 4 */
+/* USER CODE END 5 */
 
 /**
  * @brief  This function is executed in case of error occurrence.
